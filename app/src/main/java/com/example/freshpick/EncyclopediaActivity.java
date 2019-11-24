a0f844320f59a61b81d6c17efaba359458a9f047
Leon Ming <leon.ming@berkeley.edu>
2019-11-24 14:25:14 -0800

    fixed bottom navbar to not launch a new activity when clicking the same menu item

--- app/src/main/java/com/example/freshpick/EncyclopediaActivity.java
+++ app/src/main/java/com/example/freshpick/EncyclopediaActivity.java
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -62,27 +63,35 @@ public class EncyclopediaActivity extends AppCompatActivity implements Encyclope
                                                           }
                                                       });
 
-        BottomNavigationItemView groceryList = findViewById(R.id.grocery_list);
-        setMenuItemOnClick(groceryList, MainActivity.class);
-
-        BottomNavigationItemView home = findViewById(R.id.home);
-        setMenuItemOnClick(home, MainActivity.class);
-
-        BottomNavigationItemView encyclopedia = findViewById(R.id.encyclopedia);
-        setMenuItemOnClick(encyclopedia, EncyclopediaActivity.class);
-
         BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
         bottomNav.setSelectedItemId(R.id.encyclopedia);
-    }
 
-    public void setMenuItemOnClick(BottomNavigationItemView m, final Class cls) {
-        m.setOnClickListener(new View.OnClickListener() {
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View v) {
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Class cls;
+                int id = menuItem.getItemId();
+                if (id == R.id.encyclopedia) {
+                    cls = EncyclopediaActivity.class;
+                } else if (id == R.id.home) {
+                    cls = MainActivity.class;
+                } else {
+                    cls = MainActivity.class;
+                }
                 startActivity(new Intent(EncyclopediaActivity.this, cls));
+                Log.d("pageChange:", Integer.toString(menuItem.getItemId()));
+                return true;
+            }
+        });
+        bottomNav.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem menuItem) {
+                // do nothing
             }
         });
     }
+
+
 
     public void onItemClick(EncyclopediaEntry entry) {
         Intent intent = new Intent(this, DetailActivity.class);
